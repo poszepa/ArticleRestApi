a3c74e6e9a9e373585cd4844de16682824e192c8
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-08 22:42:24 +0200

    add test

--- src/test/java/com/articlerestapi/controller/ArticleControllerTest.java
+++ src/test/java/com/articlerestapi/controller/ArticleControllerTest.java
@@ -5,8 +5,10 @@ package com.articlerestapi.controller;
 import com.articlerestapi.entity.Article;
 import com.articlerestapi.entity.Author;
 import com.articlerestapi.repository.ArticleRepository;
+import com.articlerestapi.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,6 +18,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.time.LocalDate;
+import java.util.List;
+
 import static org.mockito.BDDMockito.given;
 
 
@@ -25,6 +29,7 @@ class ArticleControllerTest {
 
     @Autowired private MockMvc mockMvc;
     @MockBean private ArticleRepository articleRepository;
+    @MockBean private ArticleService articleService;
 
 
     @Test
@@ -41,5 +46,42 @@ class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$..magazine").value("test"))
                 .andDo(MockMvcResultHandlers.print());
     }
+
+
+    @Test
+    @DisplayName("Should return article list sorted by publication DESC")
+    public void getArticleListByDatePublication_success() throws Exception {
+        Author build = Author.builder().firstName("kamil").lastName("poszepczynski").build();
+        Article article = Article.builder().author(build).magazine("test").description("test").title("test").datePublication(LocalDate.of(2022, 10, 12)).build();
+        Article article2 = Article.builder().author(build).magazine("test2").description("test2").title("test2").datePublication(LocalDate.of(2021, 10, 12)).build();
+        Mockito.when(articleRepository.ListOfArtcileSortedByDatePublication()).thenReturn(List.of(article2, article));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/article/desc")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$..[0].magazine").value("test2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$..[1].magazine").value("test"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("Should return article list by key ")
+    public void getArticleListByKey_success() throws Exception {
+        Author build = Author.builder().firstName("kamil").lastName("poszepczynski").build();
+        Article article = Article.builder().author(build).magazine("test").description("test").title("test").datePublication(LocalDate.of(2022, 10, 12)).build();
+        Article article2 = Article.builder().author(build).magazine("test2").description("test super").title("test2").datePublication(LocalDate.of(2021, 10, 12)).build();
+        Mockito.when(articleService.findArticleWithKeyOnTitleOrDescription("test")).thenReturn(List.of(article2, article));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/article/findByKey/test")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$..[0].magazine").value("test2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$..[1].magazine").value("test"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+
 
 }
