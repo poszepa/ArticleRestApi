d2fcc9f071431415dc3c3821b484e4142dd8eaba
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-08 22:43:35 +0200

    add valid

--- src/main/java/com/articlerestapi/entity/Author.java
+++ src/main/java/com/articlerestapi/entity/Author.java
@@ -3,6 +3,9 @@ package com.articlerestapi.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -15,11 +18,16 @@ public class Author {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Size(min = 2, max = 20)
     @Column(name = "first_name")
     private String firstName;
 
+    @Size(min = 2, max = 20)
     @Column(name = "last_name")
     private String lastName;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE)
+    List<Article> articleList = new ArrayList<>();
 
 
 
