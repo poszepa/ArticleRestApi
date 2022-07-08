24742b27a14b3f8dafac75483690db4a485cc68a
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-08 20:09:33 +0200

    Article service to find every article by key name

---
+++ src/main/java/com/articlerestapi/service/ArticleService.java
@@ -0,0 +1,28 @@
+package com.articlerestapi.service;
+
+import com.articlerestapi.entity.Article;
+import com.articlerestapi.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    public List<Article> findArticleWithKeyOnTitleOrDescription(String searchKey) {
+        List<Article> articleList = articleRepository.findAll().stream()
+                .filter(article -> article.getTitle().toLowerCase().contains(searchKey.toLowerCase())
+                        || article.getDescription().toLowerCase().contains(searchKey.toLowerCase()))
+                .collect(Collectors.toList());
+        if(articleList.size() == 0) {
+            return new ArrayList<>();
+        }
+        return articleList;
+    }
+}
