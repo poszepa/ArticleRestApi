d4ea0912970f4c05a3780573f9f28abfa7b1bef4
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-08 08:26:29 +0200

    upload project

---
+++ src/main/java/com/articlerestapi/ArticleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.articlerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/articlerestapi/ArticleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articlerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
