e5a8f73c59f69011d02e57de3593a4ce1f1a7d78
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-07-08 22:42:09 +0200

    Add controller to add new article, edit article and remove article

--- src/main/java/com/articlerestapi/controller/ArticleController.java
+++ src/main/java/com/articlerestapi/controller/ArticleController.java
@@ -5,11 +5,11 @@ import com.articlerestapi.repository.ArticleRepository;
 import com.articlerestapi.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,18 +22,48 @@ public class ArticleController {
 
     @GetMapping("/article/{id}")
     public ResponseEntity<Article> getArticleById(@PathVariable("id")Long articleID) {
-        Article article = articleRepository.findById(articleID).orElseThrow(() -> new EntityNotFoundException("Not found article with those id"));
-        return ResponseEntity.ok(article);
+       return articleRepository.findById(articleID)
+               .map(ResponseEntity::ok)
+               .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/article/desc")
     public List<Article> getArticleListSortedByPublicationDesc() {
-        return articleRepository.ListOfArticleSortedByDatePublication();
+        return articleRepository.ListOfArtcileSortedByDatePublication();
     }
 
     @GetMapping("/article/findByKey/{key}")
     public List<Article> getArticleListWithFoundedKeyInTitleOrDescription(@PathVariable("key")String key) {
         return articleService.findArticleWithKeyOnTitleOrDescription(key);
+    }
+
+    @PostMapping("/article")
+    ResponseEntity<Article> saveArticle(@RequestBody @Valid Article article, BindingResult result) {
+        if(result.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        Article savedArticleDB = articleRepository.save(article);
+        URI savedArticle = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedArticleDB.getId())
+                .toUri();
+        return ResponseEntity.created(savedArticle).body(savedArticleDB);
+    }
+
+    @PutMapping("article/{id}")
+    ResponseEntity<?> modifyArticle(@PathVariable Long id, @RequestBody @Valid Article article, BindingResult result) {
+        if(result.hasErrors()) {
+            ResponseEntity.badRequest().build();
+        }
+        return articleService.modifyArticle(id, article)
+                .map(article1 -> ResponseEntity.noContent().build())
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("article/{id}")
+    ResponseEntity<?> removeArticle(@PathVariable("id")Long id) {
+        articleRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
